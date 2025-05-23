e90c291b5456b5a98a9e0314ff16260ee3011b5b
ThNs7 <thiagoo.nasci77@gmail.com>
2025-05-23 19:00:20 -0300

    feat: Footer and sizes.

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".whatsappButton").addEventListener("click", function() {
-  const phoneNumber = "5511986775000"; 
-  const message = "Olá, quero mais informações!"; 
-  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-  window.open(url, "_blank");
-});
